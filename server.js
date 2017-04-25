e1db59588dab4b6a286dd2a5a3bf64cb11abfffe
Marie Joyce Tio <joycetio@Joyces-MacBook-Pro.local>
2017-04-24 20:06:01 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 //dependencies 
 var express = require("express");
-var bodyParser = requre("body-parser"); 
+var bodyParser = require("body-parser");
 var path = require("path"); 
 
+//Express setup
+var app = express(); 
+var PORT = process.env.PORT || 8080; 
+
+//sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//LISTENER 
+//The below code effectivelt "starts" our server
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
